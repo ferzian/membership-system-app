06b6ebd0ba8faa3b4c68b014092185db00ade94e
ferzian <m.ferzian09@gmail.com>
2026-02-01 20:47:05 +0700

    feat: create dashboard page with layout and styling

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function DashboardPage() {
+  return (
+    <main className="max-w-md h-screen flex items-center justify-center flex-col mx-auto p-6 space-y-4 text-white">
+      <h1 className="text-2xl font-bold">Dashboard</h1>
+    </main>
+  );
+}
