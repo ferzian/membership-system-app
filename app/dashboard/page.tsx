3dcb494abad18ea058b1189a364c850c2016c553
ferzian <m.ferzian09@gmail.com>
2026-02-01 20:53:39 +0700

    feat: add session validation and redirect to sign-in in dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,10 +2,17 @@
 
 import { useRouter } from "next/navigation";
 import { useSession } from "@/lib/auth-client";
+import { useEffect } from "react";
 
 export default function DashboardPage() {
   const router = useRouter();
   const { data: session, isPending } = useSession();
+
+  useEffect(() => {
+    if (!isPending && !session?.user) {
+      router.push("/sign-in");
+    }
+  }, [isPending, session, router]);
 
   return (
     <main className="max-w-md h-screen flex items-center justify-center flex-col mx-auto p-6 space-y-4 text-white">
