79dcc057424c99aee83eb9dcd4c71e1c166bd6e5
ferzian <m.ferzian09@gmail.com>
2026-02-01 20:53:15 +0700

    feat: add session management and routing to dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,12 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+import { useSession } from "@/lib/auth-client";
+
 export default function DashboardPage() {
+  const router = useRouter();
+  const { data: session, isPending } = useSession();
+
   return (
     <main className="max-w-md h-screen flex items-center justify-center flex-col mx-auto p-6 space-y-4 text-white">
       <h1 className="text-2xl font-bold">Dashboard</h1>
